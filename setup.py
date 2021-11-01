2e3f0a62fb93b8d19f32eb4a4322b15df27b49db
CoelhoBranco <leonardo_leal15@outlook.com>
2021-11-01 20:09:00 -0300

    'atualization'

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Bromo',
+    version='0.1',
+    url='https://github.com/CoelhoBranco/bromo-lib',
+    license='MIT',
+    author='Leonardo Coelho',
+    author_email='leonardo_leal15@outlook.com',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    setup_requires=['selenium>=4.0.0'],
+   
+
+)
