30dd391e8a36db19d3e90ee0f0c24acd44634345
CoelhoBranco <leonardo_leal15@outlook.com>
2021-11-03 11:42:13 -0300

    lib lançada

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description='lib to help with page loading strategy', 
     long_description=open('README.md').read(),
     zip_safe=False,
-    setup_requires=['selenium>=4.0.0'],
+    setup_requires=['selenium>=4.0.0']
    
 
 )
